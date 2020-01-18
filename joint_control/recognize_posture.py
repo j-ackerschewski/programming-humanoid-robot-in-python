45957dba9ed5369f4d11ddb9c0a602226029a5e2
Jonathan <j.ackerschewski@campus.tu-berlin.de>
2020-01-18 18:24:21 +0100

    Implemented Server

--- joint_control/recognize_posture.py
+++ joint_control/recognize_posture.py
@@ -23,7 +23,7 @@ class PostureRecognitionAgent(AngleInterpolationAgent):
                  sync_mode=True):
         super(PostureRecognitionAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
         self.posture = 'unknown'
-        self.posture_classifier = listdir('robot_pose_data')  # LOAD YOUR CLASSIFIER
+        self.posture_classifier = listdir('../joint_control/robot_pose_data')  # LOAD YOUR CLASSIFIER
 
     def think(self, perception):
         self.posture = self.recognize_posture(perception)
